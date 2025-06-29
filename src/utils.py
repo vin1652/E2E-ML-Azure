c8ae4fbd36afebb9cf8692120f564ebf4686d312
vin1652 <136912470+vin1652@users.noreply.github.com>
2025-06-29 18:37:00 -0400

    Created a pipeline to make predictions and then used app to run the pipeline through Flask App

--- src/utils.py
+++ src/utils.py
@@ -47,3 +47,13 @@ def evaluate_model(X_train, y_train, X_test, y_test, models,params):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
+def load_object(file_path):
+    """
+    This function loads a saved object from a file.
+    """
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
